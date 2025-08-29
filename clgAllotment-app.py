1768c31e16e894a55ca60f6f02ed2204517dbc71
rahul m <165183512+mrahul01@users.noreply.github.com>
2025-08-29 15:14:45 +0530

    Initial Commit

--- clgAllotment-app.py
+++ clgAllotment-app.py
@@ -1,30 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import gdown
-
-# download_url = "https://drive.google.com/uc?export=download&id=1MZvqRk8-Ey_OLouqFLhQLaOtdSyVzkOQ"
-# allocation_results = pd.read_csv(download_url)
-
-
-# st.title("🎓 Seat Allocation Results Dashboard")
-
-# # Input: Student UniqueID
-# unique_id = st.text_input("Enter your UniqueID:")
-
-# if unique_id:
-#     student_result = allocation_results[allocation_results["UniqueID"] == unique_id]
-
-#     if not student_result.empty:
-#         st.success("✅ Seat allocated!")
-#         st.write("### Student Details")
-#         st.dataframe(student_result[[
-#             "UniqueID", "Name", "Gender", "Caste", "Rank", 
-#             "CollegeID", "Institution", "PrefNumber"
-#         ]].reset_index(drop=True))
-#     else:
-#         st.error("❌ No seat allocated for this UniqueID.")
-
-
 import streamlit as st
 import pandas as pd
 import gdown
@@ -42,9 +15,6 @@ allocation_results = pd.read_csv(output)
 
 st.title("🎓 Seat Allocation Results Dashboard")
 
-# Show all allocated seats
-st.subheader("📋 All Allocated Seats")
-st.dataframe(allocation_results)
 
 # --- Student lookup ---
 unique_id = st.text_input("Enter your UniqueID:")
